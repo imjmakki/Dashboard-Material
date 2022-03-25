932830f45022f37f84fbbeb82104f97198007bea
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-25 12:46:27 +0300

    Angular Material

--- src/app/layout/user/user.component.ts
+++ src/app/layout/user/user.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {User} from "../../interfaces/user";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-user',
@@ -19,6 +20,7 @@ export class UserComponent implements OnInit {
   dataSource = new MatTableDataSource(this.listUsers);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor() { }
 
