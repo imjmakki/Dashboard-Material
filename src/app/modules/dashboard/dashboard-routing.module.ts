ecbd4974702f2701feae66a9e8e89702fd9a09ff
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-25 10:38:18 +0300

    Angular Material

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "../../layout/dashboard/dashboard.component";
+import {MainComponent} from "../../layout/main/main.component";
+import {UserComponent} from "../../layout/user/user.component";
+import {ReportComponent} from "../../layout/report/report.component";
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, children: [
-      {  }
+      { path: '', component: MainComponent },
+      { path: 'users', component: UserComponent },
+      { path: 'reports', component: ReportComponent }
     ]}
 ];
 
