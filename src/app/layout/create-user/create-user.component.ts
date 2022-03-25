c83380f7c4b2553452058df5a6e25cb5eaa3e15f
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-25 16:28:05 +0300

    Angular Material

--- src/app/layout/create-user/create-user.component.ts
+++ src/app/layout/create-user/create-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {User} from "../../interfaces/user";
 
 @Component({
   selector: 'app-create-user',
@@ -25,5 +26,12 @@ export class CreateUserComponent implements OnInit {
 
   addUser() {
     console.log(this.form);
+
+    const user: User = {
+      user: this.form.value.user,
+      name: this.form.value.name,
+      surname: this.form.value.surname,
+      gender: this.form.value.gender,
+    }
   }
 }
