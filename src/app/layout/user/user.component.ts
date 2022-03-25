f80f55975e169937b81f8cf6c646bbc1251908ab
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-25 11:46:33 +0300

    Angular Material

--- src/app/layout/user/user.component.ts
+++ src/app/layout/user/user.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../../interfaces/user";
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
+const ELEMENT_DATA: User[] = [
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
