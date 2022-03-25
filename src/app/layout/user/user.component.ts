84d2f47fd9a20f2798d44346af53d29051e8d40a
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-25 11:50:05 +0300

    Angular Material

--- src/app/layout/user/user.component.ts
+++ src/app/layout/user/user.component.ts
@@ -3,6 +3,7 @@ import {User} from "../../interfaces/user";
 
 const ELEMENT_DATA: User[] = [
   {user: '07502078246', name: 'Mj', surname: 'Al-Anbaki', gender: 'Male'},
+  {user: '07802263519', name: 'Mohamad', surname: 'Makki', gender: 'Male'},
 ];
 
 @Component({
